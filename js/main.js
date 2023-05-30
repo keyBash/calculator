0dea15f4d8c235b90a58892a41ae78072f25e2dd
keyBash <theilya777@gmail.com>
2023-05-30 15:35:22 +0300

    create html css structure

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let a = Number 
+let b = Number
+
+
+
+let operator = null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add(a,b){
+    return a + b
+}
+
+function subtract(a,b){
+    return a - b
+}
+
+function multiplay(a,b){
+    return a * b 
+}
+
+
+function divide(a,b){
+    return a / b
+}
+
+
